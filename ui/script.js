f4ca553223174e55a683dfa69e579bcbcd30979a
Ivan Diaz <ivan@opensupports.com>
2021-02-03 01:11:10 -0300

    Add loader

--- ui/script.js
+++ ui/script.js
@@ -4,16 +4,26 @@ window.addEventListener('load', async () => {
     'href',
     `https://ropsten.etherscan.io/address/${contractAddress}`
   )
+
+  function enableLoading() {
+    document.getElementById('loader-container').setAttribute('style', 'display:block');
+  }
+  function disableLoading() {
+    document.getElementById('loader-container').setAttribute('style', 'display:none');
+  }
   // Modern dapp browsers...
   if (window.ethereum) {
       window.web3 = new Web3(ethereum);
       try {
+          enableLoading();
           await ethereum.enable();
           var accounts = await web3.eth.getAccounts();
           var accountAddress = accounts[0];
           var RouletteContract = new web3.eth.Contract(abi, contractAddress);
           
           const refreshBalance = async function () {
+            const maxBetWei = await RouletteContract.methods.getMaxBet().call();
+            const maxBet = web3.utils.fromWei(`${maxBetWei}`, 'ether');
             const currentShares = await RouletteContract.methods.getCurrentShares().call();
             const sharesOfLogged = await RouletteContract.methods.getSharesOf(accountAddress).call();
             const contractBalance = await web3.eth.getBalance(contractAddress);
@@ -23,6 +33,7 @@ window.addEventListener('load', async () => {
             const balance = _balance ? web3.utils.fromWei(`${_balance}`, 'ether') : 0;
             document.getElementById('current-liquidity').innerHTML = `${ethAtStake} ETH`;  
             document.getElementById('current-balance').innerHTML = `${balance} ETH`;
+            document.getElementById('max-bet').innerHTML = `(Max. ${maxBet} ETH)`;
           };
 
           const refreshBets = async function () {
@@ -71,6 +82,7 @@ window.addEventListener('load', async () => {
 
           await refreshBalance();
           await refreshBets();
+          disableLoading();
 
           document.getElementById('liquidity-add-button').addEventListener('click', async function () {
             const toAdd = document.getElementById('liquidity-add-input').value;
@@ -79,13 +91,17 @@ window.addEventListener('load', async () => {
               alert('Liquidity is not a number');
               return;
             }
+            enableLoading();
             await RouletteContract.methods.addLiquidity().send({from: accountAddress, value: web3.utils.toWei(toAdd, 'ether')});
             await refreshBalance();
+            disableLoading();
             alert(`Added liquidity of ${toAdd} ETH`)
           });
           document.getElementById('liquidity-remove-button').addEventListener('click', async function () {
+            enableLoading();
             await RouletteContract.methods.removeLiquidity().send({from: accountAddress});
             await refreshBalance();
+            disableLoading();
             alert(`Removed your liquidity`);
           });
           document.getElementById('place-bet').addEventListener('click', async function () {
@@ -102,6 +118,7 @@ window.addEventListener('load', async () => {
               alert(`Max bet is ${maxBet} ETH and you want to bet ${betAmount} ETH`);
               return;
             }
+            enableLoading();
             const apiResult = await RouletteContract.methods.betColor(color).send({from: accountAddress, value: web3.utils.toWei(betAmount, 'ether')});
             const result = {
               status: apiResult.events.Bet.returnValues[0],
@@ -110,9 +127,13 @@ window.addEventListener('load', async () => {
             alert(`You ${result.status}, the number thrown is ${result.thrown}`);
             await refreshBalance();
             await refreshBets();
+            disableLoading();
           });
       } catch (error) {
           // User denied account access...
+          disableLoading();
+          alert('An error occurred, check the console');
+          console.error(error);
       }
   }
   // Non-dapp browsers...
